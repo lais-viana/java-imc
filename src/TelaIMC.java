69ec88507ea0f3462074ff3d2f036a0bad66277b
Nid <vianaalveslais@gmail.com>
2025-08-03 03:01:12 -0300

    Update TelaIMC.java

--- src/TelaIMC.java
+++ src/TelaIMC.java
@@ -2,16 +2,6 @@
 import java.awt.Color;
 import javax.swing.JOptionPane;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author feuc
- */
 public class TelaIMC extends javax.swing.JFrame {
 double IMC;
     /**
